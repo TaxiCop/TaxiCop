0616ac60c963a52844b7bf0543747819afd2d593
Antonio Vanegas <hpsaturn@gmail.com>
2011-06-08 17:05:18 -0500

    -- Layout inicial request

--- src/com/taxicop/TabRequest.java
+++ src/com/taxicop/TabRequest.java
@@ -8,6 +8,8 @@ public class TabRequest extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.request_activity);
 	}
 
 }
